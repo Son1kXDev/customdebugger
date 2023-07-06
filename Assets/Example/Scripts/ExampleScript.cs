69ad43ab2dcda1fa703a23429323f5a05bf33fcf
Son1kXDev <son1kx@vk.com>
2023-07-06 05:36:00 +0300

    Few fixes

--- Assets/Example/Scripts/ExampleScript.cs
+++ Assets/Example/Scripts/ExampleScript.cs
@@ -3,37 +3,27 @@ using System.Collections;
 
 public class ExampleScript : MonoBehaviour
 {
-    // private void Start()
-    // {
-    //     Debug.Log("This is default log message!", this);
-
-    //     Debug.SetColor(Color.green);
-    //     Debug.SetStyle(Style.bold);
-    //     Debug.Log("This is custom log message!", this);
-
-    //     Debug.LogWarning("This is default warning message", this);
-
-    //     Debug.SetColor(Color.blue);
-    //     Debug.SetStyle(Style.italic);
-    //     Debug.LogWarning("This is custom warning message", this);
-
-    //     Debug.LogError("This is default error message", this);
-
-    //     Debug.SetColor(Color.cyan);
-    //     Debug.SetStyle(Style.bolditalic);
-    //     Debug.LogError("This is custom error message", this);
-
-    //     // Uncomment this to see how clearing the console works.
-    //     // Debug.Clear();
-    // }
-
-    private IEnumerator Start()
+    private void Start()
     {
+        Debug.Log("This is default log message!", this);
+
         Debug.SetColor(Color.green);
         Debug.SetStyle(Style.bold);
-        Debug.Log("Hello world!", this);
+        Debug.Log("This is custom log message!", this);
 
-        yield return new WaitForSeconds(5);
-        Debug.Clear();
+        Debug.LogWarning("This is default warning message", this);
+
+        Debug.SetColor(Color.blue);
+        Debug.SetStyle(Style.italic);
+        Debug.LogWarning("This is custom warning message", this);
+
+        Debug.LogError("This is default error message", this);
+
+        Debug.SetColor(Color.cyan);
+        Debug.SetStyle(Style.bolditalic);
+        Debug.LogError("This is custom error message", this);
+
+        // Uncomment this to see how clearing the console works.
+        // Debug.Clear();
     }
 }
